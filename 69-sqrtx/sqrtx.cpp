a6fec65d7b94c00c49038bccd588ba25a10f0766
Abdul Qadeer <148180134+sitegiga@users.noreply.github.com>
2025-10-14 03:23:37 -0700

    Time: 0 ms (100.00%) | Memory: 8.5 MB (50.61%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long l = 0, r = x, ans = 0;
+        while (l <= r) {
+            long long mid = (l + r) / 2;
+            if (mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else r = mid - 1;
+        }
+        return ans;
+    }
+};
