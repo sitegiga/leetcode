5c8ce92db76ce87312652797ecaa734962cda9bd
Abdul Qadeer <148180134+sitegiga@users.noreply.github.com>
2025-10-31 03:07:54 -0700

    Time: 10 ms (96.80%) | Memory: 149.1 MB (86.29%) - LeetSync

---
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -0,0 +1,21 @@
+class NumMatrix {
+    vector<vector<int>> dp;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = m ? matrix[0].size() : 0;
+        dp = vector<vector<int>>(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                dp[i][j] = matrix[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
+    }
+    int sumRegion(int r1, int c1, int r2, int c2) {
+        return dp[r2+1][c2+1] - dp[r1][c2+1] - dp[r2+1][c1] + dp[r1][c1];
+    }
+};
+
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
